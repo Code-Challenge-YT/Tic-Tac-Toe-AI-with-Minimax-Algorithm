1a736f76ba71ad9846608869ce20f9348fa6cb25
Mohammed <mohammed.tattan.2000@gmail.com>
2022-05-18 14:52:21 +0300

    Improve some variable names

--- main.cpp
+++ main.cpp
@@ -1,40 +1,40 @@
 #include<iostream>
 using namespace std;
 
-bool equal3(char x, char y, char z) {
+bool haveTheSameValueAndNotEmpty(char x, char y, char z) {
     if(x == y && x == z && x != ' ') {
         return true;
     }
     return false;
 }
 
-int checkWiner(char board[3][3]) {
-    //  2: X winer
-    // -2: O winer
+int checkWinner(char board[3][3]) {
+    //  2: X winner
+    // -2: O winner
     //  0: Tie
-    //  1: No winer
+    //  1: No winner
 
     // For rows
     for(int i = 0; i < 3; i++) {
-        if(equal3(board[i][0], board[i][1], board[i][2])) {
+        if(haveTheSameValueAndNotEmpty(board[i][0], board[i][1], board[i][2])) {
             return board[i][0] == 'X' ? 2 : -2;
         }
     }
 
     // For cols
     for(int i = 0; i < 3; i++) {
-        if(equal3(board[0][i], board[1][i], board[2][i])) {
+        if(haveTheSameValueAndNotEmpty(board[0][i], board[1][i], board[2][i])) {
             return board[0][i] == 'X' ? 2 : -2;
         }
     }
     
     // Diameter 1
-    if(equal3(board[0][0], board[1][1], board[2][2])) {
+    if(haveTheSameValueAndNotEmpty(board[0][0], board[1][1], board[2][2])) {
         return board[0][0] == 'X' ? 2 : -2;
     }
 
     // Diameter 2
-    if(equal3(board[2][0], board[1][1], board[0][2])) {
+    if(haveTheSameValueAndNotEmpty(board[2][0], board[1][1], board[0][2])) {
         return board[2][0] == 'X' ? 2 : -2;
     }
 
@@ -63,7 +63,7 @@ void drowBoard(char board[3][3]) {
 }
 
 int minimax(char board[3][3], int depth, bool isMaximizing, bool firstTime = true) {
-    int result = checkWiner(board);
+    int result = checkWinner(board);
     if(depth == 0 || result != 1) {
         return result;
     }
@@ -122,9 +122,9 @@ int minimax(char board[3][3], int depth, bool isMaximizing, bool firstTime = tru
 int main() {
     char board[3][3] = { {' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '} };
     int x, y; // 0, 1, 2
-    bool has_winer = false;
+    bool has_winner = false;
     char player = 'X';
-    while(!has_winer) {
+    while(!has_winner) {
         cin >> x >> y;
         if(board[x][y] == ' ') {
             board[x][y] = player;
@@ -132,16 +132,16 @@ int main() {
             int result = minimax(board, 100, false);
             drowBoard(board);
             cout << "result: " << result << "\n";
-            has_winer = checkWiner(board) != 1;
+            has_winner = checkWinner(board) != 1;
         } else {
             cout << "The field is not empty \n";
         }
     }
-    int result = checkWiner(board);
+    int result = checkWinner(board);
     if(result == 0) {
         cout << "Tie \n";
     } else {
-        cout << ((result == 2) ? "X" : "O") << ": The winer \n";
+        cout << ((result == 2) ? "X" : "O") << " player wins \n";
     }
     return 0;
 }
